aa682816a382bf330dc96761e1db2219d6d1db3e
Chanwit Piromplad <kingkong2103@gmail.com>
2018-03-01 23:49:24 +0700

    init

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) => res.send('Hello World!'))
+
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 3000
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+
+app.listen(server_port, server_ip_address, () => console.log( "Listening on " + server_ip_address + ", port " + server_port ))
